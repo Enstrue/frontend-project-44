651d11261e197e0b6d8836a0e54a711776bf55b8
Ens_code <ens.code@yandex.ru>
2023-12-26 12:49:35 +0300

    add bin/add brain-calc.js and src/index.js with general game logic

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,7 @@
-import _ from 'lodash';
 import readlineSync from 'readline-sync';
-import { sayWelcome, getUserName } from '../src/cli.js';
+import { sayWelcome, getUserName, getRandomNumber } from '../src/index.js';
 
 const isEven = (number) => number % 2 === 0;
-const getRandomNumber = () => _.random(0, 100);
 const isCorrectAnswer = (number) => (isEven(number) ? 'yes' : 'no');
 const displayGameRules = () => console.log('Answer "yes" if the number is even, otherwise answer "no".');
 const getUserAnswer = (randomNumber) => readlineSync.question(`Question: ${randomNumber}\nYour answer: `);
