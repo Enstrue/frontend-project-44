f7c68d195610351e11141fe242f3b39b012544e0
EgorTrifonov <Ens.true@gmail.com>
2023-12-26 18:02:18 +0300

    вынес логику игр в game/

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,33 +1,3 @@
-import readlineSync from 'readline-sync';
-import { sayWelcome, getUserName, getRandomNumber } from '../src/index.js';
-
-const isEven = (number) => number % 2 === 0;
-const isCorrectAnswer = (number) => (isEven(number) ? 'yes' : 'no');
-const displayGameRules = () => console.log('Answer "yes" if the number is even, otherwise answer "no".');
-const getUserAnswer = (randomNumber) => readlineSync.question(`Question: ${randomNumber}\nYour answer: `);
-
-const playRound = () => {
-  sayWelcome();
-  const userName = getUserName();
-  displayGameRules();
-
-  let correctAnswerCount = 0;
-  while (correctAnswerCount < 3) {
-    const randomNumber = getRandomNumber();
-    const userAnswer = getUserAnswer(randomNumber);
-    const correctAnswer = isCorrectAnswer(randomNumber);
-
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-      correctAnswerCount += 1;
-    } else {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${isCorrectAnswer(randomNumber)}.\nLet's try again, ${userName}`);
-      break;
-    }
-    if (correctAnswerCount === 3) {
-      console.log(`Congratulations, ${userName}!`);
-    }
-  }
-};
+import playRound from '../game/game-even.js';
 
 playRound();
