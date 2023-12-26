d6b7eccb30f581bc56a57d4e3285c80e9c73ff5e
Ens_code <ens.code@yandex.ru>
2023-12-26 17:46:57 +0300

    Вынес общую логику в src/index.js и написал логику brain-calc.js

---
+++ bin/brain-calc.js
@@ -0,0 +1,48 @@
+import readlineSync from 'readline-sync';
+import { sayWelcome, getUserName, getRandomNumber } from '../src/index.js';
+
+const displayGameRules = () => console.log('What is the result of the expression?');
+const operators = ['+', '*'];
+const getRandomOperator = () => {
+  const randomIndex = Math.floor(Math.random() * operators.length);
+  const randomOperator = operators[randomIndex];
+  return randomOperator;
+};
+const getUserAnswer = (randomNumber1, randomNumber2, randomOperator) => readlineSync.question(`Question: ${randomNumber1} ${randomOperator} ${randomNumber2}\nYour answer: `);
+
+const playRound = () => {
+  sayWelcome();
+  const userName = getUserName();
+  displayGameRules();
+
+  let correctAnswerCount = 0;
+  while (correctAnswerCount < 3) {
+    const randomNumber1 = getRandomNumber();
+    const randomNumber2 = getRandomNumber();
+    const currentOperator = getRandomOperator();
+    const userAnswer = getUserAnswer(randomNumber1, randomNumber2, currentOperator);
+    let resultExpression = 0;
+
+    switch (currentOperator) {
+      case '+':
+        resultExpression = randomNumber1 + randomNumber2;
+        break;
+      case '*':
+        resultExpression = randomNumber1 * randomNumber2;
+        break;
+      default:
+    }
+    if (+userAnswer === resultExpression) {
+      correctAnswerCount += 1;
+      console.log('Correct!');
+    } else {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${resultExpression}.\nLet's try again, ${userName}`);
+      break;
+    }
+    if (correctAnswerCount === 3) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  }
+};
+
+playRound();
--- src/cli.js
+++
@@ -1,9 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const sayWelcome = () => console.log('Welcome to the Brain Games!');
-const getUserName = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  return userName;
-};
-export { sayWelcome, getUserName };
