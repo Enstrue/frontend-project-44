7a0a4ed061c4224cc86c62bcda713cc346b7e7ab
Ens_code <ens.code@yandex.ru>
2024-01-12 17:47:00 +0300

    рефакторинг по пункту 8 замечаний

--- src/game/game-progression.js
+++ src/game/game-progression.js
@@ -2,21 +2,20 @@ import getRandomNumber from '../random-number.js';
 import playGame from '../index.js';
 
 const gameRules = 'What number is missing in the progression?';
-
-const generateQuestionAndAnswer = () => {
+const getArrayOfNumbers = () => {
   const initialNumber = getRandomNumber(0, 20);
   const progressionStep = getRandomNumber(1, 10);
+  const arrayOfNumbers = [];
+  const countOfArrayItems = getRandomNumber(5, 10);
 
-  const getArrayOfNumbers = () => {
-    const arrayOfNumbers = [];
-    const countOfArrayItems = getRandomNumber(5, 10);
+  for (let i = 0; i < countOfArrayItems; i += 1) {
+    arrayOfNumbers.push(initialNumber + i * progressionStep);
+  }
+  return arrayOfNumbers;
+};
 
-    for (let i = 0; i < countOfArrayItems; i += 1) {
-      arrayOfNumbers.push(initialNumber + i * progressionStep);
-    }
-    return arrayOfNumbers;
-  };
-
+const generateQuestionAndAnswer = () => {
+  const { initialNumber, progressionStep } = getArrayOfNumbers();
   const arrayOfProgression = getArrayOfNumbers(initialNumber, progressionStep);
   const randomIndex = getRandomNumber(0, arrayOfProgression.length - 1);
 
