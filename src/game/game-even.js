ceae89e77655a88382dc084f7fb81c9f70749461
Ens_code <ens.code@yandex.ru>
2024-01-12 17:43:50 +0300

    рефакторинг по пункту 7 замечаний

--- src/game/game-even.js
+++ src/game/game-even.js
@@ -2,12 +2,12 @@ import getRandomNumber from '../random-number.js';
 import playGame from '../index.js';
 
 const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
+const isEven = (number) => number % 2 === 0;
+const isCorrectAnswer = (number) => (isEven(number) ? 'yes' : 'no');
 
 const generateQuestionAndAnswer = () => {
   const randomNumber = getRandomNumber();
   const question = `${randomNumber}`;
-  const isEven = (number) => number % 2 === 0;
-  const isCorrectAnswer = (number) => (isEven(number) ? 'yes' : 'no');
   const correctAnswer = isCorrectAnswer(randomNumber);
   return { question, correctAnswer };
 };
